6364217e9f349396bb3437d3fea616cde517ec47
Harman Bhogal <bhogalharman7@gmail.com>
2025-04-16 17:44:04 -0700

    Added nopage and serach page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
-import { useState } from 'react'
-import './App.css'
-import { BrowserRouter } from 'react-router-dom'
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  
-
   return (
     <>
       <BrowserRouter>
-      
-      
-      
+        <Routes>
+          <Route path="/">
+            {/*Main Pages */}
+            <Route index element />
+            <Route path="Battle" />
+            <Route path="LeaderBoard" />
+            <Route path="About" />
+
+            {/*Hidden Search */}
+            <Route path="Search" />
+
+            {/*Unintended Link */}
+            <Route path="*" />
+          </Route>
+        </Routes>
       </BrowserRouter>
-      
     </>
   );
 }
 
-export default App
+export default App;
